2e88fa77c49f74bc3a2687016f1a61912886080a
riku546 <riku62560@gmail.com>
2025-08-17 23:16:46 +0900

    trpcのエラーオブジェクトを設定

--- src/app/server/trpc.ts
+++ src/app/server/trpc.ts
@@ -1,6 +1,21 @@
 import { initTRPC } from "@trpc/server";
+import { ZodError } from "zod";
 
-const t = initTRPC.create();
+const t = initTRPC.create({
+  errorFormatter(opts) {
+    const { shape, error } = opts;
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.code === "BAD_REQUEST" && error.cause instanceof ZodError
+            ? error.cause.flatten()
+            : null,
+      },
+    };
+  },
+});
 
 export const router = t.router;
 export const procedure = t.procedure;
