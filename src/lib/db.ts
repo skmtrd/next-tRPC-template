6cb955586d46a4092768c7122052a890513d5983
skmtrd <skmtgame23@gmail.com>
2025-09-24 12:05:32 +0900

    修正

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,7 +4,7 @@ import { drizzle } from "drizzle-orm/libsql";
 
 const client = createClient({
   url: process.env.TURSO_DATABASE_URL ?? "",
-  authToken: process.env.TURSO_AUTH_TOKEN,
+  authToken: process.env.TURSO_AUTH_TOKEN ?? "",
 });
 
 export const db = drizzle(client, { schema });
